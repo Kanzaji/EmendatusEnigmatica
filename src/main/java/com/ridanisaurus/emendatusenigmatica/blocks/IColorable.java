7b018a01a042462fe7ac939722859c8e29dbec62
FooterManDev <footerman.work@gmail.com>
2024-07-02 09:40:58 -0400

    Add ICOlorable

---
+++ src/main/java/com/ridanisaurus/emendatusenigmatica/blocks/IColorable.java
@@ -0,0 +1,10 @@
+package com.ridanisaurus.emendatusenigmatica.blocks;
+
+public interface IColorable
+{
+    int getHighlight2();
+    int getHighlight1();
+    int getBase();
+    int getShadow1();
+    int getShadow2();
+}

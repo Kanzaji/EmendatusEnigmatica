acd87cb8f765313a2dba937bb9da71bf5837415a
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2024-07-04 10:59:59 +0200

    Fix Armor Renderer

---
+++ src/main/java/com/ridanisaurus/emendatusenigmatica/events/ArmorTextureEvent.java
@@ -0,0 +1,80 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2020 Ridanisaurus
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package com.ridanisaurus.emendatusenigmatica.events;
+
+import com.ridanisaurus.emendatusenigmatica.renderers.ArmorTextureRenderer;
+import com.ridanisaurus.emendatusenigmatica.util.Reference;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.geom.LayerDefinitions;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.renderer.entity.ArmorStandRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.resources.PlayerSkin;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+import org.jetbrains.annotations.NotNull;
+
+@EventBusSubscriber(modid = Reference.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ArmorTextureEvent {
+    public static final ModelLayerLocation ARMOR = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "layer_1"), "main");
+    public static final ModelLayerLocation LEGS = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "layer_2"), "main");
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.@NotNull RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(ARMOR, () -> LayerDefinition.create(HumanoidModel.createMesh(LayerDefinitions.OUTER_ARMOR_DEFORMATION, 0f), 64, 32));
+        event.registerLayerDefinition(LEGS, () -> LayerDefinition.create(HumanoidModel.createMesh(LayerDefinitions.INNER_ARMOR_DEFORMATION, 0f), 64, 32));
+    }
+
+    @SubscribeEvent
+    public static void registerRenderLayers(EntityRenderersEvent.AddLayers event) {
+        event.getEntityTypes().forEach(entityType -> {
+            var entityRenderer = event.getRenderer(entityType);
+            if (entityRenderer instanceof HumanoidMobRenderer<?, ?> hmr) {
+                addRenderLayer(hmr, event.getEntityModels());
+            } else if (entityRenderer instanceof ArmorStandRenderer asr) {
+                addRenderLayer(asr, event.getEntityModels());
+            }
+        });
+        for (PlayerSkin.Model skin : event.getSkins()) {
+            EntityRenderer<?> render = event.getSkin(skin);
+            if (render instanceof PlayerRenderer pr) {
+                addRenderLayer(pr, event.getEntityModels());
+            }
+        }
+    }
+
+    private static <T extends LivingEntity, M extends HumanoidModel<T>> void addRenderLayer(LivingEntityRenderer<T, M> render, EntityModelSet models) {
+        render.addLayer(new ArmorTextureRenderer<>(render, models));
+    }
+}
---
+++ src/main/java/com/ridanisaurus/emendatusenigmatica/renderers/ArmorTextureRenderer.java
@@ -0,0 +1,101 @@
+package com.ridanisaurus.emendatusenigmatica.renderers;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.ridanisaurus.emendatusenigmatica.events.ArmorTextureEvent;
+import com.ridanisaurus.emendatusenigmatica.items.templates.BasicArmorItem;
+import com.ridanisaurus.emendatusenigmatica.util.ColorHelper;
+import com.ridanisaurus.emendatusenigmatica.util.Reference;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.client.ClientHooks;
+import org.jetbrains.annotations.NotNull;
+
+// Credit: PNC:R
+public class ArmorTextureRenderer<E extends LivingEntity, M extends HumanoidModel<E>> extends RenderLayer<E, M> {
+
+    private final HumanoidModel<E> body;
+    private final HumanoidModel<E> legs;
+
+    public ArmorTextureRenderer(RenderLayerParent<E, M> renderLayerParent, @NotNull EntityModelSet entityModelSet) {
+        super(renderLayerParent);
+        this.body = new HumanoidModel<>(entityModelSet.bakeLayer(ArmorTextureEvent.ARMOR));
+        this.legs = new HumanoidModel<>(entityModelSet.bakeLayer(ArmorTextureEvent.LEGS));
+    }
+
+    @Override
+    public void render(@NotNull PoseStack matrixStackIn, @NotNull MultiBufferSource bufferIn, int packedLightIn, @NotNull E entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        for (int i = 0; i < 5; i++) {
+            var rl = ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "textures/armor/layer_2/0"+i+ ".png");
+            var renderType = RenderType.armorCutoutNoCull(rl);
+            renderSlot(matrixStackIn, bufferIn, entity, EquipmentSlot.LEGS, packedLightIn, legs,
+                    partialTicks, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, renderType, i);
+        }
+        for (int i = 0; i < 5; i++) {
+            var rl = ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "textures/armor/layer_1/0"+i+ ".png");
+            var renderType = RenderType.armorCutoutNoCull(rl);
+            renderSlot(matrixStackIn, bufferIn, entity, EquipmentSlot.CHEST, packedLightIn, body,
+                    partialTicks, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, renderType, i);
+            renderSlot(matrixStackIn, bufferIn, entity, EquipmentSlot.FEET, packedLightIn, body,
+                    partialTicks, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, renderType, i);
+            renderSlot(matrixStackIn, bufferIn, entity, EquipmentSlot.HEAD, packedLightIn, body,
+                    partialTicks, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, renderType, i);
+        }
+    }
+
+    private void renderSlot(PoseStack matrixStack, MultiBufferSource buffer, @NotNull E entity, EquipmentSlot slot, int light, HumanoidModel<E> model, float partialTicks, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, RenderType renderType, int colorIndex) {
+        ItemStack stack = entity.getItemBySlot(slot);
+        if (stack.getItem() instanceof BasicArmorItem armor && armor.getEquipmentSlot() == slot && armor.getMaterialModel().getColors().getMaterialColor() != -1) {
+            this.getParentModel().copyPropertiesTo(model);
+            model.prepareMobModel(entity, limbSwing, limbSwingAmount, partialTicks);
+            model.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+            this.setModelSlotVisible(model, slot);
+            Model model1 = ClientHooks.getArmorModel(entity, stack, slot, model);
+            boolean glint = stack.hasFoil();
+
+            // secondary texture layer in all slots
+//            float[] secondary = decomposeColorF(armor.getColorForIndex(colorIndex));
+            this.doRender(matrixStack, buffer, light, glint, model1, armor.getColorForIndex(colorIndex), slot, renderType);
+        }
+    }
+
+    private void doRender(PoseStack matrixStack, MultiBufferSource buffer, int light, boolean glint, @NotNull Model model, int color, EquipmentSlot slot, RenderType renderType) {
+        VertexConsumer vertexConsumer = ItemRenderer.getArmorFoilBuffer(buffer, renderType, false);
+        model.renderToBuffer(matrixStack, vertexConsumer, light, OverlayTexture.NO_OVERLAY, color);
+    }
+
+    protected void setModelSlotVisible(@NotNull HumanoidModel<E> model, @NotNull EquipmentSlot slotIn) {
+        model.setAllVisible(false);
+        switch (slotIn) {
+            case HEAD -> {
+                model.head.visible = true;
+                model.hat.visible = true;
+            }
+            case CHEST -> {
+                model.body.visible = true;
+                model.rightArm.visible = true;
+                model.leftArm.visible = true;
+            }
+            case LEGS -> {
+                model.body.visible = true;
+                model.rightLeg.visible = true;
+                model.leftLeg.visible = true;
+            }
+            case FEET -> {
+                model.rightLeg.visible = true;
+                model.leftLeg.visible = true;
+            }
+        }
+    }
+}
